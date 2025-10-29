67fc624ca813af83abccf148a5603682a16f29e5
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-29 10:33:27 +0800

    log the msi commandline

--- src/ServiceHub/Protocols.vb
+++ src/ServiceHub/Protocols.vb
@@ -125,7 +125,6 @@ Public Module Protocols
                 End Sub
 
             Call pipeline.Start()
-            Call workstudio.
 
             For i As Integer = 0 To 1000
                 service = tcpPort
