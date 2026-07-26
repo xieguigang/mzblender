d7ece4d5b389382fe1851c7e99ac79c59bacd43b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-27 02:03:13 +0800

    fix of the background services startup bug

--- src/ServiceHub/Protocols.vb
+++ src/ServiceHub/Protocols.vb
@@ -1,57 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::be886f1e3952fe018f0344bd2b77a29b, mzkit\services\ServiceHub\Protocols.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 99
-    '    Code Lines: 68 (68.69%)
-    ' Comment Lines: 12 (12.12%)
-    '    - Xml Docs: 91.67%
-    ' 
-    '   Blank Lines: 19 (19.19%)
-    '     File Size: 3.43 KB
+' Summaries:
 
 
-    ' Module Protocols
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: getArgumentString, StartServer
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 99
+'    Code Lines: 68 (68.69%)
+' Comment Lines: 12 (12.12%)
+'    - Xml Docs: 91.67%
+' 
+'   Blank Lines: 19 (19.19%)
+'     File Size: 3.43 KB
+
+
+' Module Protocols
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: getArgumentString, StartServer
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -121,6 +121,12 @@ Public Module Protocols
                         tcpPort = msg.Match("\d+").DoCall(AddressOf Integer.Parse)
                     Else
                         Call msg.debug
+                    End If
+                End Sub
+            AddHandler pipeline.Finish,
+                Sub(exitCode)
+                    If tcpPort <= 0 Then
+                        Call pipeline.Start()
                     End If
                 End Sub
 
